8be5368e91a0ab3d3a20d3c4bba17aabf539535c
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-01 17:20:27 +0200

    More Scala-like: moved functionality to base Sequence class by passing type safe function

--- src/bio/sequence/ntsequence.scala
+++ src/bio/sequence/ntsequence.scala
@@ -8,20 +8,16 @@ import bio._
 
 package bio {
 
-  abstract class Sequence {
+  abstract class Sequence(fromChar: (Char) => Nucleotide,str: String) {
+    val nucleotides = str.toList.map { c => fromChar(c) }
+    override def toString() = { nucleotides mkString }
   }
 
   package DNA {
-    class Sequence(str: String) extends bio.Sequence {
-      val nucleotides = str.toList.map { nt => NucleotideConvert.fromChar(nt) }
-      override def toString() = { nucleotides mkString }
-    }
+    class Sequence(str: String) extends bio.Sequence(NucleotideConvert.fromChar,str) 
   }
 
   package RNA {
-    class Sequence(str: String) extends bio.Sequence {
-      val nucleotides = str.toList.map { nt => NucleotideConvert.fromChar(nt) }
-      override def toString() = { nucleotides mkString }
-    }
+    class Sequence(str: String) extends bio.Sequence(NucleotideConvert.fromChar,str)
   }
 }
