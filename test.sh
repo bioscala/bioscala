86ac52748b6ae9d3bef42a0a67cb3f867237726f
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-05 09:46:26 +0200

    Test script

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
+# scalac_options="-deprecation -verbose"
+scalac_options="-deprecation"
 classpath=~/.scala/scalatest-1.0.jar:/usr/share/java/jruby.jar:~/.scala/jruby-complete-1.5.0.RC1.jar
 
 echo "Compile tests"
-fsc -deprecation -cp $classpath test/ruby/bioruby_spec.scala 
-fsc -deprecation -cp $classpath test/chemistry/nucleotide_spec.scala 
-fsc -deprecation -cp $classpath test/sequence/ntsequence_spec.scala 
-fsc -deprecation -cp $classpath test/suite.scala 
+fsc $scalac_options -cp $classpath test/ruby/bioruby_spec.scala 
+fsc $scalac_options -cp $classpath test/chemistry/nucleotide_spec.scala 
+fsc $scalac_options -cp $classpath test/sequence/ntsequence_spec.scala 
+fsc $scalac_options -cp $classpath test/suite.scala 
 
 # Run the tests. The -o switch sends output to stdout.
+echo "Run tests"
 scala -cp $classpath org.scalatest.tools.Runner -p . -o -s bio.test.FullBioTestSuite
 
