d48d39ca5606247de6c1138afbd743423b736117
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-07 12:15:35 +0200

    Exclude TestSuite Run single test

--- project/build/bioscala.scala
+++ project/build/bioscala.scala
@@ -7,6 +7,9 @@ class BioScalaProject(info: ProjectInfo) extends DefaultProject(info)
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"                                                
   val scalatest = "org.scalatest" % "scalatest" % "1.0"                                               
+  override def testOptions = ExcludeTests("bio.test.FullBioTestSuite" :: Nil) :: super.testOptions.toList
+  // Modify this line to run single tests
+  override def includeTest(s: String) = { s == "bio.test.BioRubySpec" }
 }
 
 
