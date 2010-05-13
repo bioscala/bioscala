1701f56c3006623107daba7460b1123c4e622e8a
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-13 13:58:08 +0200

    Parsing Fasta

--- src/main/scala/bio/db/fasta/fastareader.scala
+++ src/main/scala/bio/db/fasta/fastareader.scala
@@ -7,11 +7,30 @@ import java.io._
 
 package bio {
 
+  class FastaReadError(string: String) extends Exception(string)
+
   class FastaReader(filename: String) extends Iterator[String] {
     private lazy val reader = new BufferedReader(new FileReader(filename))
 
     def hasNext() = reader.ready
-    def next() = reader.readLine
+    def next(): String = {
+      // Read the tag line
+      val tag = reader.readLine
+      if (tag(0) != '>')
+        throw new FastaReadError("record start expected")
+      var sequencelist = List("")
+      do {
+        reader.mark(512)
+        val line = reader.readLine
+        println(line)
+        if (line(0) == '>') {
+          reader.reset
+          return(new DNA.Sequence(tag,sequencelist.mkString).toString)
+        }
+        sequencelist += line
+      } while (reader.ready)
+      ""
+    }
     /*
     def foreach(f:String => Unit) = {
        try {
