5defffdbaa6d8da70a489900eb2ac5ca7ada63d0
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-13 10:57:18 +0200

    Alignment

---
+++ src/main/scala/bio/alignment/alignment.scala
@@ -0,0 +1,18 @@
+/**
+ * An Alignment has an (immutable) list of GappedSequences. A Sequence can
+ * consist of nucleotides, amino acids and/or gaps. Each of these can have
+ * attributes, sequences can have attributes and the Alignment can have
+ * attributes.
+ */
+
+import bio._
+import bio.attribute._
+
+package bio {
+
+  class Alignment(sequencelist: List[GappedSequence], attributelist: List[Attribute]) {
+    val list = sequencelist
+    val attributes = attributelist
+  }
+
+}
---
+++ src/test/scala/bio/alignment/alignment_spec.scala
@@ -0,0 +1,18 @@
+import bio._
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+package bio.test {
+
+  import bio.attribute._
+
+  class AlignmentSpec extends FlatSpec with ShouldMatchers {
+    "An Alignment" should "instantiate from a list" in {
+      val s1 = new DNA.GappedSequence("agc--taacg---")
+      val s2 = new DNA.GappedSequence("agc---aaca---")
+      val a = new Alignment(List(s1,s2),Nil)
+      true
+    }
+  }
+}
