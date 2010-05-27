85205ec6e05db9ddc6a44699d541dabbd538dfad
Pjotr Prins <pjotr.public01@thebird.nl>
2010-05-27 10:57:00 +0200

    Read FASTA into IUPACSequence works fine

--- src/test/scala/bio/db/fasta/fastareader_spec.scala
+++ src/test/scala/bio/db/fasta/fastareader_spec.scala
@@ -17,13 +17,21 @@ package bio.test {
     "FastaReader" should "balk on nucleotide N with standard Sequence" in {
       val f = new FastaReader("./test/data/fasta/nt.fa")
       evaluating {
-        val ids = f.map { res => 
+        val seqs = f.map { res => 
           val (id,tag,dna) = res
           new DNA.Sequence(id,tag,dna)
           }.toList
         true
       } should produce [IllegalArgumentException]
       true
+    }
+    "FastaReader" should "convert to IUPACSequence" in {
+      val f = new FastaReader("./test/data/fasta/nt.fa")
+      val seqs = f.map { res => 
+        val (id,tag,dna) = res
+        new DNA.IUPACSequence(id,tag,dna)
+        }.toList
+      seqs.head.id.toString should equal ("PUT-157a-Arabidopsis_thaliana-1")
     }
   }
 }
