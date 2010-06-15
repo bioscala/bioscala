188c2253c3614a23d9c003ccf1574c0907bd15b1
Pjotr Prins <pjotr.public01@thebird.nl>
2010-06-15 17:24:24 +0200

    Notes on translation with BioJava3

--- src/main/scala/bio/sequence/translate.scala
+++ src/main/scala/bio/sequence/translate.scala
@@ -12,13 +12,20 @@ package bio {
     /** 
      * Translate RNA to amino acids using the BioJAVA library.
      *
-     * When ambiguous symbols are used (IUPAC) the codon should
-     * translate to X. Which it apparently does with BioJava3.
+     * When ambiguous symbols are used (IUPAC) the codon 
+     * translates to X. Which it apparently does with BioJava3.
      *
-     * Also for BioJava the sequence needs consist of true
+     * Also for BioJava3 the sequence needs to consist of true
      * codons. In this function the tail is chopped to resize, 
      * unlike the EMBOSS version, which will even translate 
      * a partial codon, when possible.
+     *
+     * With translation triple gaps are translated to a single AA 
+     * gap. In fact, anything with a gap gets to be a gap. When 
+     * an unknown character is introduced the AA becomes X. So:
+     *
+     *   "agc---a-n" translates to "S--"
+     *   "agc---agn" translates to "S-X"
      */
     def translate(nucleotides: List[T]): List[Protein.AASymbol] = {
       val remove = nucleotides.size % 3
