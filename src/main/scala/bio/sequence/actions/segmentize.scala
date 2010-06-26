f0078f3329e3914540049708954c8a3c4eabaafa
Pjotr Prins <pjotr.public01@thebird.nl>
2010-06-26 13:16:42 +0200

    Codon gap

--- src/main/scala/bio/sequence/actions/segmentize.scala
+++ src/main/scala/bio/sequence/actions/segmentize.scala
@@ -13,7 +13,7 @@ package bio {
      * Split a sequence into gapped/non-gapped segments
      */
     def split(seq: List[T]): List[List[T]] = {
-      def isGapType[Q >: T](c : Q) = (c == DNA.Gap || c == RNA.Gap || c == Protein.Gap)
+      def isGapType[Q >: T](c : Q) = (c == DNA.Gap || c == RNA.Gap || c == Protein.Gap || c == Protein.CodonGap)
       def isMatch(c : T) = { if (isGapType(seq(0))) isGapType(c) else !isGapType(c) }
       val (s, tail) = seq.span{ isMatch }
       tail match {
