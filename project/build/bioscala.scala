25a5b2e026a5b3dde4d87dcec9f5642f6b87cece
Pjotr Prins <pjotr.public01@thebird.nl>
2010-07-02 11:18:57 +0200

    Added biojava-1.7.1.jar to the classpath

--- project/build/bioscala.scala
+++ project/build/bioscala.scala
@@ -3,9 +3,12 @@ import sbt._
 
 class BioScalaProject(info: ProjectInfo) extends DefaultProject(info)
 {
-  override def unmanagedClasspath = super.unmanagedClasspath +++ ("lib" / "scala" / "biojava" / "core" / "3.0-SNAPSHOT" / "core-3.0-SNAPSHOT.jar") +++ ("lib" / "scala" / "biojava" / "alignment" / "3.0-SNAPSHOT" / "alignment-3.0-SNAPSHOT.jar") 
+  override def unmanagedClasspath = super.unmanagedClasspath +++ 
+  ("lib" / "scala" / "biojava") +++
+  ("lib" / "scala" / "biojava-1.7.1.jar" ) 
+  // ("lib" / "scala" / "biojava" / "core-3.0-SNAPSHOT.jar") +++ ("lib" / "scala" / "biojava" / "alignment-3.0-SNAPSHOT.jar") +++ ("lib" / "scala" / "biojava" / "biojava3-phylo-3.0-SNAPSHOT.jar")
                                                                                                                                                      val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"                                                
-  val scalatest = "org.scalatest" % "scalatest" % "1.0"                                               
+  val scalatest = "org.scalatest" % "scalatest" % "1.0.1"                                               
   override def testOptions = ExcludeTests("bio.test.FullBioTestSuite" :: Nil) :: super.testOptions.toList
   // Modify this line to run single tests
   // override def includeTest(s: String) = { s == "bio.test.BioRubySpec" }
