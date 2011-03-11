9a2b5b328ca17cb4677798b049981192b3672a5e
Pjotr Prins <pjotr.public01@thebird.nl>
2011-03-11 14:52:29 +0100

    docs

--- scripts/generate-wikitexer.sh
+++ scripts/generate-wikitexer.sh
@@ -4,3 +4,4 @@ mkdir -p output
 rm output/*
 mkwiki -o output/bioscala-design.html --indent-env scala design.txt
 mkwiki -o output/bioscala-tutorial.html --indent-env scala tutorial.txt
+mkwiki -o output/bioscala-install.html ../INSTALL
