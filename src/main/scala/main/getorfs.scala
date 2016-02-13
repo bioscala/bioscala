719d3c4421eb41d8006c235a196b023536656057
jtnystrom <johan@monomorphic.org>
2016-02-13 16:19:22 +0900

    Spelling

--- src/main/scala/main/getorfs.scala
+++ src/main/scala/main/getorfs.scala
@@ -17,7 +17,7 @@ object GetORFApp {
       println("GetORFs "+version)
       println("""
 
-Fetch ORFs from a sequence. Sorry, this script is non-functinal at the moment.
+Fetch ORFs from a sequence. Sorry, this script is non-functional at the moment.
 
   Usage:
 
@@ -35,12 +35,12 @@ Fetch ORFs from a sequence. Sorry, this script is non-functinal at the moment.
     }
 
     type OptionMap = Map[scala.Symbol, Any]
-    
+
     def infileOption(xmap: OptionMap, s: String) : OptionMap = {
       val infiles = xmap.get( 'infiles ) match {
         case Some(l : List[String]) => s :: l
         case None => List(s)
-      } 
+      }
       Map('infiles -> infiles)
     }
     def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
@@ -48,7 +48,7 @@ Fetch ORFs from a sequence. Sorry, this script is non-functinal at the moment.
       list match {
         case Nil => map
         case "-v" :: tail    => nextOption(map ++ Map('verbose -> true), tail)
-        case string :: opt2 :: tail if switch(opt2) => 
+        case string :: opt2 :: tail if switch(opt2) =>
                                 nextOption(map ++ infileOption(map, string), list.tail)
         case string :: Nil   => nextOption(map ++ infileOption(map, string), list.tail)
         case option :: tail if switch(option) => println("Unknown option "+option)
@@ -59,29 +59,29 @@ Fetch ORFs from a sequence. Sorry, this script is non-functinal at the moment.
     }
     val options = nextOption(Map(),arglist)
 
-    def getInt(name : scala.Symbol, default : Int) : Int = 
+    def getInt(name : scala.Symbol, default : Int) : Int =
       options.get( name ) match {
         case Some(v) => v.toString.toInt
         case None    => default
       }
-    def getBool(name : scala.Symbol) : Boolean = 
+    def getBool(name : scala.Symbol) : Boolean =
       options.get( name ) match {
         case Some(_) => true
         case None    => false
       }
-   
+
     val verbose = getBool('verbose)
     println("GetORFs "+version)
 
     options.get('infiles) match {
-      case Some(l : List[String]) => 
+      case Some(l : List[String]) =>
                    l.reverse.map { infilen =>
                      if (verbose) println("Reading file ", infilen)
 //    File file = new File("tmp");
 //    if(!file.exists()) {
 //      file.mkdirs();
 //    }
-                     val f = new FastaReader(infilen) 
+                     val f = new FastaReader(infilen)
                      f.map { case (id, tag, dna) =>
                        println(id)
                      }
@@ -89,8 +89,6 @@ Fetch ORFs from a sequence. Sorry, this script is non-functinal at the moment.
       case None => println("No input files")
         sys.exit(1)
     }
-    if (verbose) println("Writing file")    
+    if (verbose) println("Writing file")
   } // main
 } // object
-
-
