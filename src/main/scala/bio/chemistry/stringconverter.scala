f000b0cfa999975e2837dea0b51d8e896ec409d3
jtnystrom <johan@monomorphic.org>
2016-04-02 18:44:40 +0900

    Fix comment

--- src/main/scala/bio/chemistry/stringconverter.scala
+++ src/main/scala/bio/chemistry/stringconverter.scala
@@ -2,16 +2,15 @@
 package bio {
 
   abstract class StringConverter[T] {
-    /** 
+    /**
      * Create an object from its character representation.
      */
-    def fromChar(c: Char): T 
-    /** 
-     * Create an object from an object - fails on Tnknown type
+    def fromChar(c: Char): T
+    /**
+     * Create an object from an object - fails on Unknown type
      */
-    def fromItem(i: T): T 
+    def fromItem(i: T): T
     def fromString(s: String): List[T] = s.toList.map { fromChar }
     def fromList(list: List[T]): List[T] = list.map { fromItem }
   } // StringConverter
 } // bio
-
